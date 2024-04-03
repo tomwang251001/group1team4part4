82b07f99f4b7818a7785957b922d795d63404b0a
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-04-03 23:28:11 +0200

    Add OkHttp incl base for MovieAPI

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
@@ -0,0 +1,8 @@
+package at.ac.fhcampuswien.fhmdb.API;
+import okhttp3.*;
+
+public class MovieAPI {
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final String URL = "http://prog2.fh-campuswien.ac.at/movies";
+    public static final String CONNECTOR = "&";
+}
