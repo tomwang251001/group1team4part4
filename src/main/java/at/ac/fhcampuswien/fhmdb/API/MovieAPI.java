f5cd4645b6d4f2dca53107629f45ca2c808ce02c
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-04-05 22:58:33 +0200

    fetch movies from API in MovieAPI

--- src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
@@ -1,12 +1,43 @@
 package at.ac.fhcampuswien.fhmdb.API;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
 import com.google.gson.*;
-public class MovieAPI{
-    /*
-    private static final OkHttpClient client = new OkHttpClient();
+import okhttp3.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static final String CONNECTOR = "&";*/
-    private static final String URL = "http://prog2.fh-campuswien.ac.at/movies";
-    Gson gson = new Gson();
+
+public class MovieAPI{
+
+    private final OkHttpClient client;
+    //public static final String CONNECTOR = "&";
+    private static final String URL = "http://prog2.fh-campuswien.ac.at";
+
+    public MovieAPI(){
+        this.client = new OkHttpClient();
+    }
+
+    public List<Movie> getRequest() throws IOException {
+        Request request = new Request.Builder()
+                .url(URL + "/movies")
+                .build();
+
+        Call call = client.newCall(request);
+        Response response = call.execute();
+
+        Gson gson = new Gson();
+
+        List<Movie> movies = new ArrayList<>();
+        JsonArray jArray = gson.fromJson(response.body().string(), JsonArray.class);
+
+        for (JsonElement element : jArray) {
+            Movie item = gson.fromJson(element, Movie.class);
+            movies.add(item);
+
+        }
+        response.close();
+        return movies;
+    }
 
 
 }
