fdceb5d30788d5b1e8c9ae953e23f6adc13bda8a
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-04-05 22:18:59 +0200

    ad Gson

--- src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
@@ -1,8 +1,12 @@
 package at.ac.fhcampuswien.fhmdb.API;
-import okhttp3.*;
-
-public class MovieAPI {
+import com.google.gson.*;
+public class MovieAPI{
+    /*
     private static final OkHttpClient client = new OkHttpClient();
+
+    public static final String CONNECTOR = "&";*/
     private static final String URL = "http://prog2.fh-campuswien.ac.at/movies";
-    public static final String CONNECTOR = "&";
+    Gson gson = new Gson();
+
+
 }
