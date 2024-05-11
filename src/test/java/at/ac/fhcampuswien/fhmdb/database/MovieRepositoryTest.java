5f0220e26141197cb7562e556ede57c2dce6d8ab
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-05-11 22:02:36 +0200

    add TODOs and WatchlistCell

---
+++ src/test/java/at/ac/fhcampuswien/fhmdb/database/MovieRepositoryTest.java
@@ -0,0 +1,28 @@
+package at.ac.fhcampuswien.fhmdb.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieRepositoryTest {
+
+    @Test
+    void test_getAllMovies() {
+        //TODO @Sascha write methode
+    }
+
+    @Test
+    void test_removeAll() {
+        //TODO @Sascha write methode
+    }
+
+    @Test
+    void test_getMovie() {
+        //TODO @Sascha write methode
+    }
+
+    @Test
+    void test_addAllMovies() {
+        //TODO @Sascha write methode
+    }
+}
