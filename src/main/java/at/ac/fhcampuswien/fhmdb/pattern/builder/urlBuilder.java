09d828263bac052e076f3e0f9673098f69a0cb5e
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-06-03 22:38:56 +0200

    write URL Builder

--- src/main/java/at/ac/fhcampuswien/fhmdb/pattern/builder/urlBuilder.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/pattern/builder/urlBuilder.java
@@ -1,4 +1,44 @@
 package at.ac.fhcampuswien.fhmdb.pattern.builder;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
+
 public class urlBuilder {
+    private static final String URL = "http://prog2.fh-campuswien.ac.at/movies";
+    private String query = null;
+    private Genre genre = null;
+    private int releaseYear = 0;
+    private int ratingFrom = 0;
+
+    public String buildURL(){
+        StringBuilder sbURL = new StringBuilder(URL);
+
+        if (query != null) sbURL.append("&query=").append(query);
+        if (genre != null) sbURL.append("&genre=").append(genre);
+        if (releaseYear != 0) sbURL.append("&releaseYear=").append(releaseYear);
+        if (ratingFrom != 0) sbURL.append("&ratingFrom=").append(ratingFrom);
+
+        return sbURL.toString();
+    }
+
+    public urlBuilder setQuery(String query){
+        this.query = query;
+        return this;
+    }
+
+    public urlBuilder setGenre(Genre genre){
+        this.genre = genre;
+        return this;
+    }
+
+    public urlBuilder setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+        return this;
+
+    }
+
+    public urlBuilder setRatingFrom(int ratingFrom) {
+        this.ratingFrom = ratingFrom;
+        return this;
+    }
+
 }
