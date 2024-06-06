bf1e25d211398c3599491a1cf8de2b4194d1f41f
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-06-06 21:45:50 +0200

    URLBuilder works

--- src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/API/MovieAPI.java
@@ -1,18 +1,13 @@
 package at.ac.fhcampuswien.fhmdb.API;
+
 import at.ac.fhcampuswien.fhmdb.Exceptions.MovieApiException;
-import at.ac.fhcampuswien.fhmdb.models.Movie;
+import at.ac.fhcampuswien.fhmdb.models.Genre;
 import at.ac.fhcampuswien.fhmdb.pattern.builder.urlBuilder;
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
 import okhttp3.*;
+
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.*;
 
-
-public class MovieAPI{
-
-    //public static final String CONNECTOR = "&";
+public class MovieAPI {
     private static final String URL = "http://prog2.fh-campuswien.ac.at/movies";
     private final OkHttpClient httpClient;
 
@@ -21,55 +16,40 @@ public class MovieAPI{
     }
 
     public String getRequest() throws MovieApiException {
-        final OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(URL)
                 .addHeader("User-Agent", "http.agent")
                 .build();
 
-        Call call = client.newCall(request);
+        Call call = httpClient.newCall(request);
         try {
             Response response = call.execute();
             return response.body().string();
         } catch (IOException ioe) {
             throw new MovieApiException("An error occurred while getRequest()", ioe);
         }
-
     }
 
-        public static String searchMovies(String query, String genre, int releaseYear, double ratingFrom) throws MovieApiException  {
+    public static String searchMovies(String query, String genre, int releaseYear, double ratingFrom) throws MovieApiException {
+        urlBuilder builder = new urlBuilder();
+        builder.setQuery(query)
+                .setGenre(Genre.valueOf(genre.toUpperCase()))
+                .setReleaseYear(releaseYear)
+                .setRatingFrom((int) ratingFrom);
 
-            String url = URL;
+        String url = builder.buildURL();
 
-            if (query != null && !query.isEmpty()) {
-                url += "?query=" + query.replace(" ", " ");
-            }
-            if (genre != null && !genre.isEmpty()) {
-                url += (url.contains("?") ? "&" : "?") + "genre=" + genre;
-            }
-            if (releaseYear > 0) {
-                url += (url.contains("?") ? "&" : "?") + "releaseYear=" + releaseYear;
-            }
-            if (ratingFrom >= 0) {
-                url += (url.contains("?") ? "&" : "?") + "ratingFrom=" + ratingFrom;
-            }
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("User-Agent", "http.agent")
+                .build();
 
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(url)
-                    .addHeader("User-Agent", "http.agent")
-                    .build();
-            try{
-                Response response = client.newCall(request).execute();
-                return response.body().string();
-            } catch (IOException e) {
-                throw new MovieApiException("error in searchMovies()", e);
-            }
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            throw new MovieApiException("Error in searchMovies()", e);
         }
-
-    public List<Movie> get() throws MovieApiException {
-        urlBuilder urlBuilder = new urlBuilder();
-        return get();
     }
-
 }
